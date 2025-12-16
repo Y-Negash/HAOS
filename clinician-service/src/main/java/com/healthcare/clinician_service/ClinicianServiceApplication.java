bb6630ada90b7abd6f3f00b7cf18d512461e98b0
Y-Negash <n.yeabsira.a@gmail.com>
2025-12-16 16:46:06 +0000

    adds clinician-service module

---
+++ clinician-service/src/main/java/com/healthcare/clinician_service/ClinicianServiceApplication.java
@@ -0,0 +1,13 @@
+package com.healthcare.clinician_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClinicianServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClinicianServiceApplication.class, args);
+	}
+
+}
---
+++ clinician-service/src/test/java/com/healthcare/clinician_service/ClinicianServiceApplicationTests.java
@@ -0,0 +1,13 @@
+//package com.healthcare.clinician_service;
+//
+//import org.junit.jupiter.api.Test;
+//import org.springframework.boot.test.context.SpringBootTest;
+//
+//@SpringBootTest
+//class ClinicianServiceApplicationTests {
+//
+//	@Test
+//	void contextLoads() {
+//	}
+//
+//}
