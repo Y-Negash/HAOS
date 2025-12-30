e383ef7110cbdce5ed71bea3ba14f62354293a3d
Y-Negash <n.yeabsira.a@gmail.com>
2025-12-30 16:14:30 +0000

    creates patient repo extending JPARepository

---
+++ patient-service/src/main/java/com/healthcare/patient_service/repository/PatientRepository.java
@@ -0,0 +1,10 @@
+package com.healthcare.patient_service.repository;
+
+import com.healthcare.patient_service.model.Patient;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface PatientRepository extends JpaRepository<Patient, Long> {
+
+}
